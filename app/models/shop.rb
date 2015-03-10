9f6772c551fa9b7eb6fd817dba8459f607fee18b
NGUYEN Thi Quynh Nga <quynhnga.k59spt@gmail.com>
2015-03-10 05:46:03 +0100

    added some validates in model shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,15 @@ class Shop < ActiveRecord::Base
 	# acts_as_copy_target
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
+
+	validates :chain, length: { maximum: 40 }
+	validates :name, length: { maximum: 100 }
+	validates :address, length: { maximum: 100 }
+	validates :city, length: { maximum: 40 }
+	validates :country_code, length: { maximum: 2 }
+
+	validates :name, :presence => true
+	validates :address, :presence => true
+	validates :phone, :presence => true
+
 end
